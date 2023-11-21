6ca16277a56b7a445f08a588230aeca6808e8ceb
Elias Renawi <elias.renawi@bizi.co.il>
2023-11-21 17:00:22 +0200

    added projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+import React, { useRef } from 'react'
+import SectionHeading from './SectionHeading'
+import { projectsData } from '@/lib/data';
+
+import Project from './Project';
+
 
 const Projects = () => {
+
+    //const { ref } = useSectionInView("Projects", 0.5);
+    const ref = useRef(null)
     return (
-        <div>Projects</div>
-    )
+        <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+            <SectionHeading>My projects</SectionHeading>
+            <div>
+                {projectsData.map((project, index) => (
+                    <React.Fragment key={index}>
+                        <Project {...project} />
+                    </React.Fragment>
+                ))}
+            </div>
+        </section>
+    );
 }
 
 export default Projects
